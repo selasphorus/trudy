307bfc272e6932b375bb9692732c68034eb366d1
atc <birdhive@gmail.com>
2023-09-26 17:35:37 -0400

    cont.; NTS

--- template-parts/content/entry_loop_footer.php
+++ template-parts/content/entry_loop_footer.php
@@ -11,6 +11,7 @@
 <footer class="entry-footer">
 	<?php
 	if ( !is_post_type_archive('newsletter') ) {
+		// Link to full post TMP disabled for newsletters until HTML content is enabled
 		get_template_part( 'template-parts/content/entry_actions', get_post_type() );
 	}
 	?>
