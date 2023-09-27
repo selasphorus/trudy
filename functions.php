48f88e119c4c624f588c598bb6fd92e84158bd0a
atc <birdhive@gmail.com>
2023-09-27 13:46:04 -0400

    Trying to get child styles to load

--- functions.php
+++ functions.php
@@ -10,8 +10,9 @@ define( 'TEMPLATE_URL', get_stylesheet_directory_uri() );
 add_action( 'wp_enqueue_scripts', 'site_scripts_and_styles' );
 function site_scripts_and_styles() {
     
+    //wp_enqueue_style( 'trudy-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    //wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ) );
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ) );
     //wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/styles/style.css', array( 'parent-style' ) );
 	
     // Events Manager (EM) style overrides and additions
