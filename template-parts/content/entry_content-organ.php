0de2cb29c535517c054223e772e34b3b770e7d87
atc <birdhive@gmail.com>
2024-06-25 17:32:55 -0400

    cont.

--- template-parts/content/entry_content-organ.php
+++ template-parts/content/entry_content-organ.php
@@ -37,7 +37,7 @@ namespace Kadence;
 	do_action( 'kadence_single_after_entry_content' );
 
 	if ( queenbee() ) {
-		$settings = array( 'fields' => array( 'builder', 'model', 'opus_num', 'build_year', 'venue_filename', 'num_manuals', 'num_divisions', 'num_ranks', 'num_stops', 'num_pipes', 'num_registers', 'num_other', 'action_type', 'organ_sum_html', 'organ_html', 'divisions_html', 'stops_summary' ) );
+		$settings = array( 'fields' => array( 'builder', 'model', 'opus_num', 'build_year', 'venue_filename', 'num_manuals', 'num_divisions', 'num_ranks', 'num_stops', 'num_pipes', 'num_registers', 'num_other', 'organ_sum_html', 'organ_html', 'specs_html', 'stops_summary', 'action_type' ) );
 		acf_form( $settings );	
 	}
 	
